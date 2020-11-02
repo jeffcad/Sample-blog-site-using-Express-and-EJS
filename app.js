0464418e3cc339b0ff1ccb49e7e7ef219993962e
Jeff Cadieux <jeffcadieux@hotmail.com>
2020-11-02 16:12:15 +0900

    Challenge18 Use Lodash on post title & route param

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const lodash = require('lodash');
 
 const homeStartingContent = 'Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.';
 const aboutContent = 'Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.';
@@ -46,8 +47,10 @@ app.post('/compose', (req, res) => {
 });
 
 app.get('/posts/:postName', (req, res) => {
+  const requestedTitle = lodash.lowerCase(req.params.postName);
   for (const post of posts) {
-    if (post.title === req.params.postName) {
+    const postTitle = lodash.lowerCase(post.title);
+    if (postTitle === requestedTitle) {
       console.log('Match found!');
       break;
     };
